483ca236a0a767a3fba51c785b59c39aab61ca1a
AppuMali <126770741+AppuMali@users.noreply.github.com>
2023-03-07 08:20:23 -0500

    Add files via upload

---
+++ PropApp.jsx
@@ -0,0 +1,9 @@
+import React from "react";
+import PropNav from "./PropComponents/PropNav";
+
+const PropApp = ()=>{
+    return(
+        < PropNav data = "Appu" />
+    )
+}
+export default PropApp;
---
+++ PropNav.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const PropNav =(props)=>{
+    console.log(props)
+    return(
+        <>
+            <h1>{props.data}</h1>
+        </>
+    )
+}
+export default PropNav;
